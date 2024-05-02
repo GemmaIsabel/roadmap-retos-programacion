0a0bb3600467fda3150893616cf404a1cbc41566
GemmaIsabel <168694838+GemmaIsabel@users.noreply.github.com>
2024-05-01 22:58:20 -0400

    Update GemmaIsabel.js

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/GemmaIsabel.js
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/GemmaIsabel.js
@@ -1,1 +1,19 @@
+// https://www.javascript.com
 
+// comentario en una linea
+
+/* 
+comentario en varias lineas
+*/
+
+let variable = 'variable';
+
+const mybirthday = '08.09.1983';
+
+let edad = 40;
+let num = 1.2345;
+
+let alert = true;
+
+let imprimir = '¡Hola, javascript!'
+console.log (imprimir);
